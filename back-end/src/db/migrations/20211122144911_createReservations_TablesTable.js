87519360efcfa091776665bb7469512dadb38837
DevenGibbs <devengibbs91@gmail.com>
2021-11-22 14:54:33 -0600

    updated migrations and seeds from 'seats' to 'tables' for test purposes

--- back-end/src/db/migrations/20211122144911_createReservations_TablesTable.js
+++ back-end/src/db/migrations/20211122144911_createReservations_TablesTable.js
@@ -1,23 +1,24 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable("reservations_seats", (table) => {
-        table.increments("reservation_seat_id").primary();
+    return knex.schema.createTable("reservations_tables", (table) => {
+        table.increments("reservation_table_id").primary();
         table.integer("reservation_id").unsigned().notNullable();
         table
             .foreign("reservation_id")
             .references("reservation_id")
             .inTable("reservations")
             .onDelete("CASCADE");
-        table.integer("seat_id").unsigned().notNullable();
+        table.integer("table_id").unsigned().notNullable();
         table
-            .foreign("seat_id")
-            .references("seat_id")
-            .inTable("seats")
+            .foreign("table_id")
+            .references("table_id")
+            .inTable("tables")
             .onDelete("CASCADE");
         table.timestamps(true, true);
       });
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTable("reservations_seats");
+    return knex.schema.dropTable("reservations_tables");
 };
+
--- back-end/src/db/seeds/01-seats.js
+++
@@ -1,7 +0,0 @@
-const seatsData = require("./01-seats.json");
-
-exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE seats RESTART IDENTITY CASCADE")
-    .then(() => knex("seats").insert(seatsData));
-};
