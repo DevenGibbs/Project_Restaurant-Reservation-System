35719d4ddd863d565db7b0a6f5578eeca94e1f9c
DevenGibbs <devengibbs91@gmail.com>
2021-12-06 12:15:57 -0600

    updated REACT_APP_API_BASE_URL for deployment attempt

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -7,7 +7,7 @@ import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
-//Attempting to edit deployment by connecting to heroku deployed back-end using config variables on dashbaord
+//Attempting to get to working deployment by connecting to heroku deployed back-end using REACT_APP_API_BASE_URL
 
 /**
  * Defines the default headers for these functions to work with `json-server`
