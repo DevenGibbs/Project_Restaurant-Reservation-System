6a6203f54a77796fbd3df4cd85e71d8db2959ca3
DevenGibbs <devengibbs91@gmail.com>
2021-12-06 12:22:18 -0600

    updated API_BASE_URL for deployment attempt

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,8 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+  process.env.REACT_APP_API_BASE_URL 
+  // || "http://localhost:5000";
 //Attempting to get to working deployment by connecting to heroku deployed back-end using REACT_APP_API_BASE_URL
 
 /**
